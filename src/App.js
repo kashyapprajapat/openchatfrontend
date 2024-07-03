95e5cd01aa668f0c58f23164eb3df68218f1af4a
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-07-03 13:41:30 +0530

    push code to github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { nanoid } from "nanoid"
+
+const socket = io(process.env.REACT_APP_CHAT_BACKEND_URL);
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+  const [sentMessages, setSentMessages] = useState(new Set());
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    if (!username) {
+      const inputUsername = prompt("Please enter your name:");
+      if (inputUsername) {
+        setUsername(inputUsername);
+        const messageId = nanoid(4);
+        setSentMessages((prev) => new Set(prev).add(messageId));
+        socket.emit("chat", { id: messageId, username: inputUsername, message });
+      } else {
+        alert("Username is required to send a message.");
+      }
+    } else {
+      const messageId = nanoid(4);
+      setSentMessages((prev) => new Set(prev).add(messageId));
+      socket.emit("chat", { id: messageId, username, message });
+    }
+    setMessage('');
+  };
+
+  // useEffect(() => {
+  //   socket.on("chat", (payload) => {
+  //     if (!sentMessages.has(payload.id)) {
+  //       setChat((prevChat) => [...prevChat, payload]);
+  //     }
+  //   });
+  // }, [sentMessages]);
+
+  useEffect(() => {
+    socket.on('chat', (payload) => {
+      if (!sentMessages.has(payload.id)) {
+        setChat((prevChat) => [...prevChat, payload]);
+      }
+    });
+
+    return () => {
+      socket.off('chat');
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Open chat 💬🫂</h1>
+
+        {chat.map((payload, index) => (
+          <p key={index}><strong>{payload.username}:</strong> {payload.message}</p>
+        ))}
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="Type your message here..."
+            value={message}
+            autoFocus
+            autoComplete='off'
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
       </header>
     </div>
   );
